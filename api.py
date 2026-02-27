c70f6f73c8f706ff263bb7f3d8b9e2b7585b6ea0
rtgsepoct <ttrbtechnology444@gmail.com>
2026-02-27 03:19:16 +0100

    api prediction update part 2

--- api.py
+++ api.py
@@ -57,5 +57,51 @@ def predict():
             predicted_sentiment = single_prediction(predictor, scaler, cv, text_input)
 
             return jsonify({"prediction":prediction_sentiment})
-        
-        
+    except Exception as e: 
+        return jsonify({"error": str(e)})
+    
+
+def single_prediction(predictor, scaler, cv, text_input):
+    corpus = []
+    stemmer = PorterStemmer()
+    review = re.sub("[^a-zA-Z]", " ", text_input)
+    review = review.lower().split()
+    review = [stemmer.stem(word for word in review if not word in STOPWORDS)]
+    review = " ".join(review)
+    corpus.append(review)
+    X_prediction = cv.transform(corpus).toarray()
+    X_prediction_scl = scaler.tranform(X_prediction)
+    y_prediction = predictor.predict_proba(X_prediction_scl)
+    y_prediction = y_predictions.argmax(axis=1)[0] 
+
+
+    return "Positive" if y_predictions ==1 else  "Negative"
+
+def bulk_prediction(predictor, scaler, cv, data):
+    def bulk_prediction(predictor, scaler, cv, data):
+        corpus = []
+        stemmer = PorterStemmer ()
+        for i in range(0, data. shape[0]):
+            review = re.sub("[^a-zA-Z]".
+            , "", data. iloc[i][ "Sentence"])
+            review = review. lower(). split()
+            review = [stemmer.stem(word) for word in review if not word in STOPWORDS]
+            review = " ".join(review)
+            corpus. append (review)
+
+    X_prediction = cv.transform(corpus).toarray()
+    X_prediction_scl = scaler.transform(X_prediction)
+    y_predictions = predictor.predict_proba(X_prediction_sc1)
+    y_predictions = y_predictions.argmax(axis=1)
+    y_predictions = list(map(sentiment_mapping, y_predictions))
+
+    data[ "Predicted sentiment"] = y_predictions
+    predictions_csv = BytesIO()
+
+    data. to_csv(predictions_csv, index=False) 
+    predictions_csv.seek(0)
+
+    graph = get_distribution_graph(data)
+    return predictions_csv, graph
+
+
