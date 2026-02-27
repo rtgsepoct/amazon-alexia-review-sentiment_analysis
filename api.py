5f3f8a4d760341ba3e6e7de3f6a4ff978e33a08c
rtgsepoct <ttrbtechnology444@gmail.com>
2026-02-27 03:00:39 +0100

    api prediction part1

--- api.py
+++ api.py
@@ -9,3 +9,53 @@ import pandas as pd
 import pickle 
 import base64
 
+STOPWORDS = set(stopwords.words("english"))
+
+app = Flask(__name__)
+
+@app.route("./test", method=["GET"])
+def test():
+    return "Teset request received successfully. Service is running"
+
+
+@app.route("/", methods = ["GET", "POST"])
+def home():
+    return render_template("landing.html")
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # select the predictor.to be loaded from Models: 
+    predictor  = pickle.load(open(r"./model_dt.pkl", "rb"))
+    scaler = pickle.load(open(r"./scaler.pkl", 'rb'))
+    cv = pickle.load(open(r"./countVectorizer.pkl", "rb"))
+    try:
+        #check if the request contains a file (for bulk predictions) or text input
+        if "file" in request.files:
+            #Bulk prediction from CSV file
+            file = request.files['file']
+            data = pd.read_csv(file)
+
+            predictions, graph = bulk_prediction(predictor, scaler, cv, data)
+
+            response = send_file(
+                predictions, 
+                mimetype="text/csv"
+                as_attachment=True
+                download_name = "Predictions.csv"
+            )
+
+            response.header["X-Graph-Exists"] = "true"
+
+            response.headers["X-Graph-Data"] = base64.b64encode(
+                graph.getbuffer()
+            ).decode("ascii")
+
+            return response
+        elif "text" in request.json:
+            # Single string predictoin 
+            text_input  = request.json["text"]
+            predicted_sentiment = single_prediction(predictor, scaler, cv, text_input)
+
+            return jsonify({"prediction":prediction_sentiment})
+        
+        
